48772dcada479014fe660ebdc72032e98052623f
Daniel Florez <danielflorez111@gmail.com>
2021-01-16 16:00:56 -0500

    first commit

---
+++ 01-rts/src/index.tsx
@@ -0,0 +1,22 @@
+import ReactDOM from 'react-dom';
+
+// import GuestList from './state/GuestList';
+// import UserSearch from './state/UserSearch';
+// import EventComponent from './events/EventComponent';
+import UserSearch from './refs/UserSearch';
+
+const App = () => {
+    return (
+        <div>
+            {/* <GuestList /> */}
+            {/* <UserSearch /> */}
+            {/* <EventComponent /> */}
+            <UserSearch />
+        </div>
+    )
+};
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
